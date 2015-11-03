ba483bd3c9ab6e49aa2ab873c2a2e455f63124d1
jude89654 <jude89654@gmail.com>
2015-11-03 12:33:18 +0800

    update

--- src/com/ust/edu/controller/ListItemServlet.java
+++ src/com/ust/edu/controller/ListItemServlet.java
@@ -1,6 +1,8 @@
 package com.ust.edu.controller;
 
 import java.io.IOException;
+import java.sql.Connection;
+
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,32 +10,42 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.ust.edu.utility.sql.SQLOperations;
+
 /**
  * Servlet implementation class ListItemServlet
  */
 @WebServlet("/ListItem.html")
 public class ListItemServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
+	Connection connection;
 	/**
 	 * @see Servlet#init(ServletConfig)
 	 */
 	public void init(ServletConfig config) throws ServletException {
-		// TODO Auto-generated method stub
+		super.init(config);
+			connection = SQLOperations.getConnection();
+		
+		if (connection != null) {
+			getServletContext().setAttribute("dbConnection", connection);
+			System.out.println("connection is READY.");
+		} else {
+			System.err.println("connection is NULL.");
+		}
 	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		doPost(request,response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
 	}
 
 }
